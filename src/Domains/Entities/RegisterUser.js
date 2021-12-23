4ffe80c922013a64adb50df2c9c1ad1af509ffa6
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-23 13:35:22 +0700

    add RegisterUserEntities

---
+++ src/Domains/Entities/RegisterUser.js
@@ -0,0 +1,19 @@
+class RegisterUser{
+  constructor(payload){
+    const {email, password, fullname} = this.verifyPayload(payload);
+
+    this.email = email;
+    this.password = password;
+    this.fullname = fullname;
+    this.hashedPassword = null;
+  }
+  verifyPayload({email, password, fullname}){
+    if(!email || !password || !fullname){
+      throw new Error('Register_User.Not_Contain_Data_Spesification');
+    }
+    if(typeof(email)!=='string' || typeof(password)!== 'string' ||typeof(fullname)!=='string'){
+      throw new Error('Register_User.Not_Meet_Data_Spesification')
+    }
+    return({email, password, fullname})
+  }
+}
