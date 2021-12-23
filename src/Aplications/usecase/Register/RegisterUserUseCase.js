e042589c63bea7a3d09f0b6b005fac3f1956f150
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-23 14:11:02 +0700

    implement validation registerUser

--- src/Aplications/usecase/Register/RegisterUserUseCase.js
+++ src/Aplications/usecase/Register/RegisterUserUseCase.js
@@ -2,12 +2,14 @@ const RegisterUser = require('../../../Domains/RegisterUsers/Entities/RegisterUs
 const RegisteredUser = require('../../../Domains/RegisterUsers/Entities/RegisteredUser');
 
 class RegisterUserUseCase{
-  constructor({registerRepository, passwordHash, tokenManager}){
+  constructor({registerRepository, passwordHash, tokenManager, validation}){
     this.registerRepository = registerRepository;
     this.passwordHash = passwordHash;
     this.tokenManager = tokenManager;
+    this.validation = validation;
   }
   async execute(payload){
+    this.validation.validateRegisterUserPayload(payload);
     const registerUser = new RegisterUser(payload);
     await this.registerRepository.verifyAvailableEmail(payload.email);
     registerUser.hashedPassword = await this.passwordHash.hashPassword(payload.pasword);
