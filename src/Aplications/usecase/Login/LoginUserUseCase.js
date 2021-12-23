926935f85137dc55a4890112cf2f2f9c89fd4dee
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-23 20:23:15 +0700

    add LoginUserUseCase

---
+++ src/Aplications/usecase/Login/LoginUserUseCase.js
@@ -0,0 +1,23 @@
+const LoginUser = require('../../../Domains/LoginUser/Entities/LoginUser')
+const LogedinUser = require('../../../Domains/LoginUser/Entities/LogedinUser')
+
+class LoginUserUseCase{
+  constructor({loginUserRepository, passwordHash, tokenManager, validation}){
+    this.loginUserRepository = loginUserRepository;
+    this.passwordHash = passwordHash;
+    this.tokenManager = tokenManager;
+    this.validation = validation;
+  }
+  async execute(payload){
+    this.validation.validateLoginUserPayload(payload);
+    const loginUser = new LoginUser(payload);
+    await this.loginUserRepository.verifyUserByEmail(loginUser.email);
+    const user = await this.loginUserRepository.getUserByEmail(loginUser.email);
+    await this.passwordHash.comparePassword(loginUser.password, user.password);
+    loginUser.id = user.id;
+    const token = await this.tokenManager.createToken(loginUser);
+    return new LogedinUser({...loginUser, user, token});
+  }
+}
+
+module.exports = LoginUserUseCase;
