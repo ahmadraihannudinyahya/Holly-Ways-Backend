50e1808a1bc8f26f2cfb95f0181d73cc75bcc85e
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-23 13:39:22 +0700

    add RegisterRepository

---
+++ src/Domains/RegisterUsers/RegisterRepository.js
@@ -0,0 +1,8 @@
+class RegisterRepository{
+  async verifyAvailableEmail(){
+    throw new Error('RegisterRepository is abstract class');
+  }
+  async addUser(){
+    throw new Error('RegisterRepository is abstract class');
+  }
+}
