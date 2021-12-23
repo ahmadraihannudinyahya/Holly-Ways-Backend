bd6ef470ba8a5e63c6d26498982aed442fd718f3
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-23 19:01:45 +0700

    add keyword super JwtTokenManager

--- src/Infrastrutures/Security/JwtTokenManager.js
+++ src/Infrastrutures/Security/JwtTokenManager.js
@@ -2,6 +2,7 @@ const TokenManager = require("../../Aplications/Security/TokenManager");
 
 class JwtTokenManager extends TokenManager{
   constructor(jwt){
+    super();
     this.jwt = jwt;
   }
   async createToken({id}){
