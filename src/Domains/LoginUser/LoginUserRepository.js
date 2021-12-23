08bdbd0cd6ceb155adb9bb705e2479d224abb9f3
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-23 20:07:29 +0700

    add LoginUserRepository

---
+++ src/Domains/LoginUser/LoginUserRepository.js
@@ -0,0 +1,10 @@
+class LoginUserRepository{
+  async verifyUserByEmail(){
+    throw new Error('LoginUserRepository is abstract class');
+  }
+  async getUserByEmail(){
+    throw new Error('LoginUserRepository is abstract class');
+  }
+}
+
+module.exports = LoginUserRepository;
