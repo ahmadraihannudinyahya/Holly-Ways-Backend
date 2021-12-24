490f4de5579e618ace9f4cdd9ba55eb1124643b2
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-24 09:54:07 +0700

    add endpoint deletuser in userinterface

--- src/Interfaces/Http/Api/User/Handler.js
+++ src/Interfaces/Http/Api/User/Handler.js
@@ -1,10 +1,12 @@
 const GetAllUserUseCase = require('../../../../Aplications/usecase/User/GetAllUserUseCase');
+const DeleteUserByIdUseCase = require('../../../../Aplications/usecase/User/DeleteUserByIdUseCase');
 
 class UserHandler {
   constructor(container) {
     this.container = container;
 
     this.getAllUserHandler = this.getAllUserHandler.bind(this);
+    this.deleteUserByIdHandler = this.deleteUserByIdHandler.bind(this);
   }
 
   async getAllUserHandler(req, res, next) {
@@ -14,6 +16,23 @@ class UserHandler {
       res.send({
         data: {
           users,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async deleteUserByIdHandler(req, res, next) {
+    try {
+      const authHeader = req.header('Authorization');
+      const token = authHeader && authHeader.split(' ')[1];
+      const deleteUserByIdUseCase = this.container.getInstance(DeleteUserByIdUseCase.name);
+      await deleteUserByIdUseCase.execute({ token, ...req.params });
+      res.send({
+        status: 'success',
+        data: {
+          id: req.params.userId,
         },
       });
     } catch (error) {
--- src/Interfaces/Http/Api/User/route.js
+++ src/Interfaces/Http/Api/User/route.js
@@ -3,6 +3,7 @@ const express = require('express');
 const route = (handler) => {
   const router = express.Router();
   router.get('/user', handler.getAllUserHandler);
+  router.delete('/user/:userId', handler.deleteUserByIdHandler);
   return router;
 };
 module.exports = route;
