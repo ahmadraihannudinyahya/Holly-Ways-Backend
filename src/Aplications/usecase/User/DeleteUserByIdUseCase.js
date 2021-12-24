884f8d1d78627ee5d0c0abe70d928e1103a22a3e
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-24 09:28:05 +0700

    add deleteUserByIdUseCase

---
+++ src/Aplications/usecase/User/DeleteUserByIdUseCase.js
@@ -0,0 +1,13 @@
+class DeleteUserByIdUseCase {
+  constructor({ tokenManager, userRepository }) {
+    this.tokenManager = tokenManager;
+    this.userRepository = userRepository;
+  }
+
+  async execute(payload) {
+    const logedinuser = await this.tokenManager.verivyToken(payload.token);
+    await this.userRepository.verifyUserDeleteSelf(payload.userId, logedinuser);
+    this.userRepository.deleteUserById(payload.userId);
+  }
+}
+module.exports = DeleteUserByIdUseCase;
