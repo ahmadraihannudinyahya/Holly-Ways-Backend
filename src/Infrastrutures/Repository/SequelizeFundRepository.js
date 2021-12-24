82fe0c600e10fa41ea9ca1da2a31fe737fe42dc4
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-24 21:47:47 +0700

    add method in sequelizeFundRepository

--- src/Infrastrutures/Repository/SequelizeFundRepository.js
+++ src/Infrastrutures/Repository/SequelizeFundRepository.js
@@ -1,4 +1,6 @@
 const FundRepository = require('../../Domains/Fund/FundRepository');
+const AuthorizationError = require('../../Commons/Exceptions/AuthorizationError');
+const NotFoundError = require('../../Commons/Exceptions/NotFoundError');
 
 class SequelizeFundRepository extends FundRepository {
   constructor(Funds, idGenerator) {
@@ -15,6 +17,38 @@ class SequelizeFundRepository extends FundRepository {
 
   async getAllFund() {
     return this.Funds.findAll();
+  }
+
+  async verifyFundOwner(fundId, ownerId) {
+    const fund = await this.Funds.findOne({
+      where: {
+        id: fundId,
+      },
+    });
+
+    if (fund.owner !== ownerId) {
+      throw new AuthorizationError('Only owner can delete');
+    }
+  }
+
+  async deleteFundById(id) {
+    this.Funds.destroy({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async verifyFundFound(id) {
+    const fund = await this.Funds.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!fund) {
+      throw new NotFoundError('Fund not found');
+    }
   }
 }
 
