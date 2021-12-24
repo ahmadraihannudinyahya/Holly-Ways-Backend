8ab9f7c424744d8cbeb895506810ff66b69127e6
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-24 14:48:17 +0700

    add AddFundUseCase

---
+++ src/Aplications/usecase/Fund/AddFundUseCase.js
@@ -0,0 +1,23 @@
+const NewFund = require('../../../Domains/Fund/Entities/NewFund');
+const AddedFund = require('../../../Domains/Fund/Entities/AddedFund');
+
+class AddFundUseCase {
+  constructor({
+    tokenManager, validation, storageService, fundRepository,
+  }) {
+    this.tokenManager = tokenManager;
+    this.validation = validation;
+    this.storageService = storageService;
+    this.fundRepository = fundRepository;
+  }
+
+  async execute(payload) {
+    const { userId: owner } = await this.tokenManager.verifyToken(payload.token);
+    await this.validation.validateNewFundPayload(payload);
+    const newFund = new NewFund({ ...payload, owner });
+    newFund.setThumbnail = await this.storageService.uploadFile(thumbnail);
+    newFund.fundId = this.fundRepository.addFund(newFund);
+    return new AddedFund(newFund);
+  }
+}
+module.exports = AddFundUseCase;
