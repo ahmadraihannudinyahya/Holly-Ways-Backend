41b53985891986f41604f9ecc0aafb7bbbe86e10
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-24 09:17:25 +0700

    add verifyToken in TokenManager

--- src/Aplications/Security/TokenManager.js
+++ src/Aplications/Security/TokenManager.js
@@ -2,5 +2,9 @@ class TokenManager {
   async createToken() {
     throw new Error('TokenManager is abstract class');
   }
+
+  async verivyToken() {
+    throw new Error('TokenManager is abstract class');
+  }
 }
 module.exports = TokenManager;
