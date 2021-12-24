d62876fac3861b00fd83dd5ae3e4d5a787b4f530
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-24 22:00:08 +0700

    add params in endpoint delete fund

--- src/Interfaces/Http/Api/Fund/route.js
+++ src/Interfaces/Http/Api/Fund/route.js
@@ -4,7 +4,7 @@ const route = (handler, midleware) => {
   const router = express.Router();
   router.post('/fund', midleware.fileHandle('thumbnail'), handler.addFundHandler);
   router.get('/fund', handler.getAllFundHandler);
-  router.delete('/fund', handler.deleteFundByIdHandler);
+  router.delete('/fund/:fundId', handler.deleteFundByIdHandler);
   return router;
 };
 module.exports = route;
