a1e8796b586fe488c96f393239d8e80366a39a18
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-25 14:12:20 +0700

    add req.file for usecase param

--- src/Interfaces/Http/Api/Donation/Handler.js
+++ src/Interfaces/Http/Api/Donation/Handler.js
@@ -11,7 +11,9 @@ class DonationHandler {
       const authHeader = req.header('Authorization');
       const token = authHeader && authHeader.split(' ')[1];
       const addDonationUseCase = this.container.getInstance(AddDonationUseCase.name);
-      const id = await addDonationUseCase.execute({ ...req.body, ...req.params, token });
+      const id = await addDonationUseCase.execute({
+        ...req.body, ...req.params, proofAttachment: req.file, token,
+      });
       res.send({
         status: 'success',
         data: {
