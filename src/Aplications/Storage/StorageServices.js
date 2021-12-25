b2ae2757d4d86ad7505843f9c9fefeb8dc7075ea
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-25 09:12:41 +0700

    add method deleteFile in StorageServices

--- src/Aplications/Storage/StorageServices.js
+++ src/Aplications/Storage/StorageServices.js
@@ -2,6 +2,10 @@ class StorageServices {
   uploadFile() {
     throw new Error('StorageServices is abstract class');
   }
+
+  deleteFile() {
+    throw new Error('StorageServices is abstract class');
+  }
 }
 
 module.exports = StorageServices;
