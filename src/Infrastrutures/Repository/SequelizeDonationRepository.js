7c2363c8199e81539ea48b367c300b5162527563
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-25 15:18:57 +0700

    add method in SequelizeDonationRepository

--- src/Infrastrutures/Repository/SequelizeDonationRepository.js
+++ src/Infrastrutures/Repository/SequelizeDonationRepository.js
@@ -1,4 +1,6 @@
 const DonationRepository = require('../../Domains/Donations/DonationRepository');
+const NotFoundError = require('../../Commons/Exceptions/NotFoundError');
+const AuthorizationError = require('../../Commons/Exceptions/AuthorizationError');
 
 class SequelizeDonationRepository extends DonationRepository {
   constructor(idGenerator, Donations) {
@@ -11,6 +13,36 @@ class SequelizeDonationRepository extends DonationRepository {
     const id = `donation-${this.idGenerator()}`;
     await this.Donations.create({ ...newDonation, id });
     return id;
+  }
+
+  async setStatusSuccessDonation(id) {
+    this.Donations.update({ status: 'success' }, {
+      where: {
+        id,
+      },
+    });
+  }
+
+  async verifyDonationFound(id) {
+    const donation = await this.Donations.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!donation) {
+      throw new NotFoundError('Donation not Found');
+    }
+  }
+
+  async verifyDonationInFund(donationId, fundId) {
+    const donation = await this.Donations.findOne({
+      where: {
+        id: donationId,
+      },
+    });
+    if (donation.fundId !== fundId) {
+      throw new AuthorizationError('Donation must in Fund');
+    }
   }
 }
 module.exports = SequelizeDonationRepository;
