f79350a6c22621bf73699f56c95f9d357ba9142d
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-25 15:36:44 +0700

    change response object

--- src/Interfaces/Http/Api/Donation/Handler.js
+++ src/Interfaces/Http/Api/Donation/Handler.js
@@ -35,7 +35,9 @@ class DonationHandler {
       await setStatusSuccessDonationUseCase.execute({ ...req.params, token });
       res.send({
         status: 'success',
-        id: req.params.donationId,
+        data : {
+          id: req.params.donationId,
+        }
       });
     } catch (error) {
       next(error);
