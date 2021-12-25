21ee163929fe7f49d64a194377513b51ad5b0b9e
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-25 20:16:19 +0700

    add associate model Donation User

--- models/donations.js
+++ models/donations.js
@@ -11,6 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Donations.belongsTo(models.Users, {
+        as: "user",
+        foreignKey: {
+          name: "userId",
+        },
+      });
     }
   };
   Donations.init({
