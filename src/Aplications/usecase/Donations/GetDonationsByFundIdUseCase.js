56b13db5f6eaa765387c9419ae5034b828af19ce
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-25 19:35:41 +0700

    add GetDonationsByFundIdUseCase

---
+++ src/Aplications/usecase/Donations/GetDonationsByFundIdUseCase.js
@@ -0,0 +1,28 @@
+const GetDonation = require('../../../Domains/Donations/Entities/GetDonation');
+
+class GetDonationsByFundIdUseCase {
+  constructor({
+    tokenManager, userRepository, fundRepository, donationsRepository,
+  }) {
+    this.tokenManager = tokenManager;
+    this.userRepository = userRepository;
+    this.fundRepository = fundRepository;
+    this.donationsRepository = donationsRepository;
+  }
+
+  async execute(payload) {
+    const { userId } = this.tokenManager.verifyToken(payload.token);
+    await this.userRepository.verifyUserFound(userId);
+    await this.fundRepository.verifyFundFound(payload.fundId);
+    try {
+      await this.fundRepository.verifyFundOwner(payload.fundId, userId);
+      const donations = await this.donationsRepository.getAllFundByFundId(payload.fundId);
+      return donations.map((donation) => new GetDonation(donation));
+    } catch {
+      const donations = await this.donationsRepository.getSuccessFundByFundId(payload.fundId);
+      return donations.map((donation) => new GetDonation(donation));
+    }
+  }
+}
+
+module.exports = GetDonationsByFundIdUseCase;
