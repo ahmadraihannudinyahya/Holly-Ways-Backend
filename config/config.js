2e503db307de6ae506d496952be3f9e3fd2d7a2c
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-26 10:07:30 +0700

    change config file database from env

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+module.exports = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port" : process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT
+  },
+  "test": {
+    "username": process.env.DBTEST_USER,
+    "password": process.env.DBTEST_PASSSWORD,
+    "database": process.env.DBTEST_NAME,
+    "host": process.env.DBTEST_HOST,
+    "port" : process.env.DBTEST_PORT,
+    "dialect": process.env.DBTEST_DIALECT
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+};
