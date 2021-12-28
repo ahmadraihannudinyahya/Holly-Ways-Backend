4e2cb928b89bde4d2b4fcc18410f5a5f94d56f4a
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-28 08:49:16 +0700

    change test create server and remove hello world feature

--- src/Infrastrutures/Http/_test/createServer.test.js
+++ src/Infrastrutures/Http/_test/createServer.test.js
@@ -2,11 +2,19 @@ const request = require("supertest");
 const createServer = require('../createServer');
 const container = require('../../Container');
 
-describe("Test the root path", () => {
-  it('shoild response 200', async ()=>{
+describe('create server test', ()=>{
+  it('should return app object corectly', ()=>{
     const app = createServer(container);
-    const response = await request(app).get('/');
-    expect(response.statusCode).toEqual(200);
-    expect(response.text).toEqual('Hello World!');
+    expect(app).toBeDefined();
+  });
+  it('should handle error corectly', async()=>{
+    const app = createServer({});
+    const response = await request(app).get('/unregisteredroute');
+    expect(response.statusCode).toEqual(404);
+  });
+  it('should response 500 server error corectly', async ()=>{
+    const app = createServer({});
+    const response = await request(app).get('/api/v1/fund');
+    expect(response.statusCode).toEqual(500);
   })
-});
+})
