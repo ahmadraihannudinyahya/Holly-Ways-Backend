05759c1cf072dbc8507eed653b959857fba60e6b
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-29 08:50:03 +0700

    fix bug type of 0 is undef

--- src/Domains/Fund/Entities/GetFund.js
+++ src/Domains/Fund/Entities/GetFund.js
@@ -8,16 +8,21 @@ class GetFund {
     this.thumbnail = thumbnail;
     this.goal = goal;
     this.description = description;
+    this.donationObtained = donationObtained;
   }
 
   verifyPayload({
     id, title, thumbnail, goal, description, donationObtained
   }) {
-    if (!id || !title || !thumbnail || !goal || !description || !donationObtained) {
-      throw new Error('Added_Fund.Not_Contain_Data_Spesification');
+    if (!id || !title || !thumbnail || !goal || !description ) {
+      if ( !donationObtained && donationObtained !== 0 ){
+        throw new Error('Get_Fund.Not_Contain_Data_Spesification');
+      }
     }
-    if (typeof (id) !== 'string' || typeof (title) !== 'string' || typeof (thumbnail) !== 'string' || typeof (description) !== 'string' || typeof (goal) !== 'number' || typeof (donationObtained) !=='number') {
-      throw new Error('Added_Fund.Not_Meet_Data_Spesification');
+    if (typeof (id) !== 'string' || typeof (title) !== 'string' || typeof (thumbnail) !== 'string' || typeof (description) !== 'string' || typeof (goal) !== 'number' ) {
+      if (typeof (donationObtained) !== 'number' && typeof (donationObtained) !== 'boolean'){
+        throw new Error('Get_Fund.Not_Meet_Data_Spesification');
+      }
     }
     return ({
       id, title, thumbnail, goal, description, donationObtained
