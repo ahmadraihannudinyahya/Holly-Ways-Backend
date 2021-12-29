443d81a9f7d1f3de8d46e62a733d5b90834e88bf
raihan <ahmad.raihannudin.yahya@gmail.com>
2021-12-29 07:59:18 +0700

    change GetFund entities

--- src/Domains/Fund/Entities/GetFund.js
+++ src/Domains/Fund/Entities/GetFund.js
@@ -1,7 +1,7 @@
 class GetFund {
   constructor(payload) {
     const {
-      id, title, thumbnail, goal, description,
+      id, title, thumbnail, goal, description, donationObtained
     } = this.verifyPayload(payload);
     this.id = id;
     this.title = title;
@@ -11,16 +11,16 @@ class GetFund {
   }
 
   verifyPayload({
-    id, title, thumbnail, goal, description,
+    id, title, thumbnail, goal, description, donationObtained
   }) {
-    if (!id || !title || !thumbnail || !goal || !description) {
+    if (!id || !title || !thumbnail || !goal || !description || !donationObtained) {
       throw new Error('Added_Fund.Not_Contain_Data_Spesification');
     }
-    if (typeof (id) !== 'string' || typeof (title) !== 'string' || typeof (thumbnail) !== 'string' || typeof (description) !== 'string' || typeof (goal) !== 'number') {
+    if (typeof (id) !== 'string' || typeof (title) !== 'string' || typeof (thumbnail) !== 'string' || typeof (description) !== 'string' || typeof (goal) !== 'number' || typeof (donationObtained) !=='number') {
       throw new Error('Added_Fund.Not_Meet_Data_Spesification');
     }
     return ({
-      id, title, thumbnail, goal, description,
+      id, title, thumbnail, goal, description, donationObtained
     });
   }
 }
