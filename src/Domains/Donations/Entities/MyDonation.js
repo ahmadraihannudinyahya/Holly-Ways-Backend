4f94a96410f2be27f44d36fd9e8f94e7acd7d027
raihan <ahmad.raihannudin.yahya@gmail.com>
2022-01-01 14:07:59 +0700

    add mydonation entities

---
+++ src/Domains/Donations/Entities/MyDonation.js
@@ -0,0 +1,37 @@
+class MyDonation{
+  constructor(payload){
+    const {id, donateAmount, status, proofAttachment, createdAt, fundTitle} = this.verifyPayload(payload);
+    this.id = id;
+    this.donateAmount = donateAmount;
+    this.status = status;
+    this.proofAttachment = proofAttachment;
+    this.createdAt = this.convertDate(createdAt);
+    this.fundTitle = fundTitle
+  }
+  verifyPayload({id, donateAmount, status, proofAttachment, createdAt, fundTitle}){
+    if (!id || !donateAmount || !status || !proofAttachment ||!createdAt || !fundTitle) {
+      throw new Error('My_Donation.Not_Contain_Data_Spesification');
+    }
+    if (
+      typeof (id) !== 'string'
+      || typeof (donateAmount) !== 'number'
+      || typeof (status) !== 'string'
+      || typeof (proofAttachment) !== 'string'
+      || typeof (createdAt) !== 'object'
+      || typeof (fundTitle) !== 'string'
+    ) {
+      throw new Error('My_Donation.Not_Meet_Data_Spesification');
+    }
+    return ({
+      id, donateAmount, status, proofAttachment, createdAt, fundTitle
+    });
+  }
+  convertDate(date){
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const day = days[date.getDay()];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let month = months[date.getMonth()];
+    return `${day}, ${date.getDate()} ${month} ${date.getFullYear()}`;
+  }
+}
+module.exports = MyDonation;
