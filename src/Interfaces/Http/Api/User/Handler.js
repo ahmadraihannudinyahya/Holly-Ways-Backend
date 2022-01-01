2ab30d78484edeaa401097e4010013eafff70174
raihan <ahmad.raihannudin.yahya@gmail.com>
2022-01-01 19:38:19 +0700

    add endpoint profil in user interaface

--- src/Interfaces/Http/Api/User/Handler.js
+++ src/Interfaces/Http/Api/User/Handler.js
@@ -1,5 +1,6 @@
 const GetAllUserUseCase = require('../../../../Aplications/usecase/User/GetAllUserUseCase');
 const DeleteUserByIdUseCase = require('../../../../Aplications/usecase/User/DeleteUserByIdUseCase');
+const GetProfilUseCase = require('../../../../Aplications/usecase/User/GetProfilUseCase');
 
 class UserHandler {
   constructor(container) {
@@ -7,6 +8,7 @@ class UserHandler {
 
     this.getAllUserHandler = this.getAllUserHandler.bind(this);
     this.deleteUserByIdHandler = this.deleteUserByIdHandler.bind(this);
+    this.getProfilHandler = this.getProfilHandler.bind(this);
   }
 
   async getAllUserHandler(req, res, next) {
@@ -36,6 +38,23 @@ class UserHandler {
           id: req.params.userId,
         },
       });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getProfilHandler(req, res, next){
+    try {
+      const authHeader = req.header('Authorization');
+      const token = authHeader && authHeader.split(' ')[1];
+      const getProfilUseCase = this.container.getInstance(GetProfilUseCase.name);
+      const user = await getProfilUseCase.execute({token});
+      res.send({
+        status : 'success',
+        data : {
+          user
+        }
+      })
     } catch (error) {
       next(error);
     }
--- src/Interfaces/Http/Api/User/route.js
+++ src/Interfaces/Http/Api/User/route.js
@@ -4,6 +4,7 @@ const route = (handler) => {
   const router = express.Router();
   router.get('/user', handler.getAllUserHandler);
   router.delete('/user/:userId', handler.deleteUserByIdHandler);
+  router.get('/profil', handler.getProfilHandler);
   return router;
 };
 module.exports = route;
