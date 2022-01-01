b816c66fadd3336f07adbc183c3f2a1ef08369da
raihan <ahmad.raihannudin.yahya@gmail.com>
2022-01-01 10:41:38 +0700

    add donation obtained as params implement getFund in GetMyFundUseCase

--- src/Aplications/usecase/Fund/GetMyFundUseCase.js
+++ src/Aplications/usecase/Fund/GetMyFundUseCase.js
@@ -8,7 +8,22 @@ class GetMyFundUseCase{
   async execute(payload){
     const { userId } = await this.tokenManager.verifyToken(payload.token);
     const funds = await this.fundRepository.getFundsByOwner(userId);
-    return funds.map(fund => GetFund(fund));
+    return funds.map(fund => {
+      return new GetFund({
+        id : fund.id, 
+        title : fund.title,
+        thumbnail : fund.thumbnail, 
+        goal : fund.goal, 
+        description : fund.description, 
+        createdAt : fund.createdAt, 
+        donationObtained : fund.donations.reduce((total, donation) => {
+          if( donation.fundId === fund.id ){
+            return total + donation.donateAmount;
+          };
+          return total;
+        }, 0)
+      })
+    });
   }
 }
 module.exports = GetMyFundUseCase;
