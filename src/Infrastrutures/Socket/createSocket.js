7334418d2d6fbdde28862797ac1aae2041bd55b3
raihan <ahmad.raihannudin.yahya@gmail.com>
2022-01-07 07:10:58 +0700

    add create socket and interface socket

---
+++ src/Infrastrutures/Socket/createSocket.js
@@ -0,0 +1,12 @@
+const {Server} = require('socket.io')
+
+const createSocket = (server) =>{
+  const io = new Server(server, {
+    cors : {
+      origin : '*'
+    }
+  });
+  require('./../../Interfaces/Socket/Notification')(io);
+}
+
+module.exports = createSocket;
