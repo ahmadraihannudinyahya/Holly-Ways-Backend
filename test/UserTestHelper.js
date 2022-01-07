fd6f1ffd4f636358afd0c080b537f531cd73c802
raihan <ahmad.raihannudin.yahya@gmail.com>
2022-01-07 19:51:23 +0700

    add unit test SequelieRegisterRepository

--- test/UserTestHelper.js
+++ test/UserTestHelper.js
@@ -4,6 +4,20 @@ const UserTestHelper = {
     await Users.destroy({
       where : {},
     });
+  },
+
+  async getUserById(id){
+    return Users.findOne({
+      where : {
+        id
+      }
+    });
+  },
+
+  async addUser({id = 'user-123', email = 'test@mail.com', fullname = 'user test', password = 'supersecretpass'}){
+    await Users.create({
+      id, email, fullname , password
+    });
   }
 }
 
