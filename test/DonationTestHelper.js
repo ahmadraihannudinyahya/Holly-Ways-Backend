83dc6ade88f5f94a7c7f24a7bc735159457bfd15
raihan <ahmad.raihannudin.yahya@gmail.com>
2022-01-08 15:52:24 +0700

    add test SequelizeDonationRepository

--- test/DonationTestHelper.js
+++ test/DonationTestHelper.js
@@ -4,6 +4,28 @@ const DonationTestHelper = {
     await Donations.destroy({
       where : {},
     })
-  }
+  },
+
+  async getDonationById(id){
+    return Donations.findOne({
+      where : {
+        id
+      }
+    })
+  },
+
+  async addDonation({id = 'donation-123', donateAmount = 20000, fundId = 'fund-123', proofAttachment = 'image.jpg', userId = 'user-123'}){
+    await Donations.create({
+      id, donateAmount, fundId, proofAttachment, userId
+    });
+  },
+
+  async setStatusDonationById(id){
+    await Donations.update({ status: 'success' }, {
+      where: {
+        id,
+      },
+    });
+  },
 }
 module.exports = DonationTestHelper
