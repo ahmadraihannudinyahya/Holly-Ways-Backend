86bca246106fb35f7fa4184ac6d812f5e05b018f
raihan <ahmad.raihannudin.yahya@gmail.com>
2022-01-09 10:46:06 +0700

    add test DeleteUserByIdUseCase

---
+++ src/Aplications/usecase/User/_test/DeleteUserByIdUseCase.test.js
@@ -0,0 +1,32 @@
+const DeleteUserByIdUseCase = require('../DeleteUserByIdUseCase');
+
+const TokenManager = require('../../../Security/TokenManager');
+const UserRepository = require('../../../../Domains/User/UserRepository');
+
+describe('test DeleteUserByIdUseCase', ()=>{
+  it('should orchestrating DeleteUserByIdUseCase corectly', async ()=>{
+    payload = {
+      token : 'test.token',
+      userId : 'user-123'
+    };
+    const mockTokenManager = new TokenManager();
+    const mockUserRepository = new UserRepository();
+
+    mockTokenManager.verifyToken = jest.fn()
+      .mockImplementation(()=> Promise.resolve({userId : payload.userId}));
+    mockUserRepository.verifyUserDeleteSelf = jest.fn()
+      .mockImplementation(()=>Promise.resolve());
+    mockUserRepository.deleteUserById = jest.fn()
+      .mockImplementation(()=> Promise.resolve());
+
+    const deleteUserByIdUseCase = new DeleteUserByIdUseCase({
+      tokenManager : mockTokenManager,
+      userRepository : mockUserRepository,
+    });
+    await deleteUserByIdUseCase.execute(payload);
+
+    expect(mockTokenManager.verifyToken).toBeCalledWith(payload.token);
+    expect(mockUserRepository.verifyUserDeleteSelf).toBeCalledWith(payload.userId, payload.userId);
+    expect(mockUserRepository.deleteUserById).toBeCalledWith(payload.userId);
+  });
+});
